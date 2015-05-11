15f26dd14e29d2224aa9f0f509cc0b01b3bc2fb0
carloscpossa <carlos_cpossa@hotmail.com>
2015-05-11 11:15:58 -0300

    Ajax Cidades
    Alteração no ajax das cidades variando o estado

--- classes/cidades.ajax.php
+++ classes/cidades.ajax.php
@@ -4,9 +4,11 @@
 	require_once("cidades.php");
 	require_once("estados.php");
 	
-	$cod_estados = mysql_real_escape_string( $_REQUEST['cod_estados'] );	
+	//$cod_estados = mysql_real_escape_string( $_REQUEST['cod_estados'] );	
 	
-	$cod_estados = 11;
+	$cod_estados = $_REQUEST['cod_estados'];	
+	
+	//$cod_estados = 11;
 	$est = new Estado();
 	$est = $est->pesquisaEstadoById($cod_estados);
 	
