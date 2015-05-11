18d8ea799677a3538ee71c242ab00796b1bfefa8
carloscpossa <carlos_cpossa@hotmail.com>
2015-05-11 15:36:20 -0300

    Bootstrap
    inclusão do bootstrap na página dos dados do participante

--- dadosparticipante.php
+++ dadosparticipante.php
@@ -45,6 +45,18 @@
 			echo "<title>".$part->getNomeCompleto()." - Desenvolvimento de Sistemas para a Web</title>";
 		?>
         <link rel="stylesheet" href="estilos/estilo.css" />
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">        
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
         <header>
@@ -79,5 +91,9 @@
         <footer>
             <small>Autor: Carlos Henrique Coimbra Possa - 2014</small>
         </footer>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     </body>
 </html>
