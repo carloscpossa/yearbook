1e1cdc8259f0e278dc43d0960427f736eca02fcb
carloscpossa <carlos_cpossa@hotmail.com>
2015-05-11 14:58:12 -0300

    CSS
    Alteração no CSS e inclusão de tag meta no html

--- cabecalhoLogin.php
+++ cabecalhoLogin.php
@@ -11,6 +11,7 @@
         <meta charset="utf-8" lang="pt-br" />
         <title>Yearbook - Desenvolvimento de Aplicações Web - PUC 2014</title>
         <link rel="stylesheet" href="estilos/estilo.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">        
